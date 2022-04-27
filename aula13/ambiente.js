b05a2e557317986909475d805426d94abebb9b3f
JoaoNer <johnnercastrojy@gmail.com>
2022-04-27 10:52:13 -0300

    aula 13

---
+++ aula13/ambiente.js
@@ -0,0 +1,22 @@
+var contador = 1
+do {
+    console.log(`Número ${contador} `)
+    contador++ //contador = contador + 1        TESTE LÓGICO NO FINAL
+} while (contador <= 6)
+
+
+/*var contador = 1
+while (contador <= 6){
+    console.log(`Número ${contador} `)
+    contador++ //contador = contador + 1        TESTE LÓGICO NO ÍNICIO
+}
+*/
+
+/*
+console.log('Tudo bem')
+console.log('Tudo bem')
+console.log('Tudo bem')     FORMA ERRADA DE SE FAZER REPETIÇÃO
+console.log('Tudo bem')
+console.log('Tudo bem')
+console.log('Tudo bem')
+*/
