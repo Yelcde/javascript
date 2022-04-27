bda67692c3d88b334bf696ffee80c917052a958c
JoaoNer <johnnercastrojy@gmail.com>
2022-04-27 14:19:40 -0300

    .

--- aula12ex/modelo/javascript.js
+++
@@ -1,8 +0,0 @@
-function carregar() {
-    var msg = document.getElementById('msg')
-    var img = document.getElementById('foto')
-    var agora = new Date()
-    var hora = agora.getHours()
-    msg.innerHTML = `Agora são ${hora} horas.`
-
-}
